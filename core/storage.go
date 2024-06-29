3036c4794ff9186f89043e95215eab19ebb65e12
Blu.Fi Tech, Inc <blufitech@gmail.com>
2024-06-29 16:28:13 -0400

    Update

--- core/storage.go
+++ core/storage.go
@@ -1,16 +1,31 @@
 package core
 
 type Storage interface {
-	Put(*Block) error
+    Put(*Block) error
+    Get(hash []byte) (*Block, error)
 }
 
 type MemoryStore struct {
+    store map[string]*Block
 }
 
-func NewMemorystore() *MemoryStore {
-	return &MemoryStore{}
+func NewMemoryStore() *MemoryStore {
+    return &MemoryStore{
+        store: make(map[string]*Block),
+    }
 }
 
 func (s *MemoryStore) Put(b *Block) error {
-	return nil
+    // Assuming the block has a method Hash() that returns its hash as a string
+    hash := string(b.Hash())
+    s.store[hash] = b
+    return nil
+}
+
+func (s *MemoryStore) Get(hash []byte) (*Block, error) {
+    b, ok := s.store[string(hash)]
+    if !ok {
+        return nil, fmt.Errorf("block not found")
+    }
+    return b, nil
 }
