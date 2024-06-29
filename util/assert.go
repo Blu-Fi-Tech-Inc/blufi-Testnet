229ce297706464559ee850936cdf65f9cdbb5a7f
Blu.Fi Tech, Inc <blufitech@gmail.com>
2024-06-29 03:22:45 -0400

    Update

--- util/assert.go
+++ util/assert.go
@@ -1,58 +1,12 @@
 package util
 
 import (
-	"math/rand"
-	"testing"
-	"time"
-
-	"github.com/blu-fi-tech-inc/boriqua_project/core"
-	"github.com/blu-fi-tech-inc/boriqua_project/crypto"
-	"github.com/blu-fi-tech-inc/boriqua_project/types"
-	"github.com/stretchr/testify/assert"
+	"log"
+	"reflect"
 )
 
-func RandomBytes(size int) []byte {
-	token := make([]byte, size)
-	rand.Read(token)
-	return token
-}
-
-func RandomHash() types.Hash {
-	return types.HashFromBytes(RandomBytes(32))
-}
-
-// NewRandomTransaction return a new random transaction whithout signature.
-func NewRandomTransaction(size int) *core.Transaction {
-	return core.NewTransaction(RandomBytes(size))
-}
-
-func NewRandomTransactionWithSignature(t *testing.T, privKey crypto.PrivateKey, size int) *core.Transaction {
-	tx := NewRandomTransaction(size)
-	assert.Nil(t, tx.Sign(privKey))
-	return tx
-}
-
-func NewRandomBlock(t *testing.T, height uint32, prevBlockHash types.Hash) *core.Block {
-	txSigner := crypto.GeneratePrivateKey()
-	tx := NewRandomTransactionWithSignature(t, txSigner, 100)
-	header := &core.Header{
-		Version:       1,
-		PrevBlockHash: prevBlockHash,
-		Height:        height,
-		Timestamp:     time.Now().UnixNano(),
+func AssertEqual(a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		log.Fatalf("ASSERTION: %+v != %+v", a, b)
 	}
-	b, err := core.NewBlock(header, []*core.Transaction{tx})
-	assert.Nil(t, err)
-	dataHash, err := core.CalculateDataHash(b.Transactions)
-	assert.Nil(t, err)
-	b.Header.DataHash = dataHash
-
-	return b
-}
-
-func NewRandomBlockWithSignature(t *testing.T, pk crypto.PrivateKey, height uint32, prevHash types.Hash) *core.Block {
-	b := NewRandomBlock(t, height, prevHash)
-	assert.Nil(t, b.Sign(pk))
-
-	return b
 }
