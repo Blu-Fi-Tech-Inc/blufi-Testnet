ed975fdf198cd943f3a91357230b5169a73ff130
Blu.Fi Tech, Inc <blufitech@gmail.com>
2024-07-03 06:54:11 -0400

    Update

---
+++ crypto/signature.go
@@ -0,0 +1,26 @@
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"math/big"
+)
+
+// Signature represents an ECDSA signature with R and S values.
+type Signature struct {
+	R, S *big.Int
+}
+
+// Sign computes the ECDSA signature of the provided data using the given private key.
+func Sign(privKey *PrivateKey, data []byte) (*Signature, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, privKey.PrivateKey, data)
+	if err != nil {
+		return nil, err
+	}
+	return &Signature{R: r, S: s}, nil
+}
+
+// VerifySignature verifies an ECDSA signature given the public key, data, and signature.
+func VerifySignature(pubKey *PublicKey, data []byte, signature *Signature) bool {
+	return ecdsa.Verify(pubKey.PublicKey, data, signature.R, signature.S)
+}
